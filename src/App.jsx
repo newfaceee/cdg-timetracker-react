d73ef0e49a7abf61181d20da7066ec5c83ea6820
Edem <50662754+newfaceee@users.noreply.github.com>
2020-09-12 18:18:09 +0300

    Меняет разметку компонента App на jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header, Main } from "./components";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default App;
